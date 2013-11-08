45b8d77e10dc087341832854446b99e5ff5ecf9a
Chao Yan <chaoyan@cs.ubc.ca>
2013-11-08 00:39:43 -0800

    forget to include some top-level files 	new file:   cra_cfg.m 	new file:   cra_close.m 	new file:   cra_open.m

---
+++ cra_close.m
@@ -0,0 +1,23 @@
+% CRA_CLOSE: Release all CRA resources 
+%   cra_close;
+function cra_close
+	disp('Close Java process');
+	java_close;
+	disp('Close logger'); 
+	log_close;
+	%cra_rmpath; % do not remove path
+	disp('CRA resources have been released!');
+end %function cra_close
+
+function cra_rmpath
+	cra_home = cra_info('cra_home'); 
+	cra_dirs = cra_info('cra_dirs'); 
+  disp('Remove CRA directories from Matlab search path');
+  %disp('Remove the following directories from Matlab search path');
+  for i=1:length(cra_dirs)
+    dirname = [cra_home,'/',cra_dirs{i}];
+    %disp(sprintf('  > %s',dirname));
+    rmpath(dirname); 
+  end
+  % rmpath(cra_home);  % leave the cra_home to call cra_open
+end %function cra_rmpath
